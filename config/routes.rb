c024e0f91983ec040579b82282634cba8c8e5eab
ktw1222 <ktw1222@gmail.com>
2015-08-10 17:05:57 -0400

    changed confirm#new to confirm#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/welcome', to: 'welcome#show'
   get '/details', to: 'details#new'
-  get '/confirm', to: 'confirm#new'
+  get '/confirm', to: 'confirm#show'
   post '/confirm', to: 'confirm#create'
   get '/done', to: 'done#show'
 
