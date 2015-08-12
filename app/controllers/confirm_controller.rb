6a3b9a1a9545c3b4325d0de8d2e6c3552dcec139
ktw1222 <ktw1222@gmail.com>
2015-08-12 15:05:49 -0400

    added confirm controller

--- app/controllers/confirm_controller.rb
+++ app/controllers/confirm_controller.rb
@@ -1,9 +1,18 @@
 class ConfirmController < ApplicationController
 
-  def show
+  def create
+    @reservation = Reservation.create!(reservation_params)
+
   end
 
-  def create
+  def show
+    @reservation = Reservation.find(params[:reservation_id])
+    @room = @reservation.room.find(params[:room_id])
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:reservation_date)
   end
 
 end
--- app/controllers/done_controller.rb
+++ app/controllers/done_controller.rb
@@ -1,6 +1,8 @@
 class DoneController < ApplicationController
 
   def show
+    @reservation = Reservation.find(params[:reservation_id])
+    @room = @reservation.room.find(params[:room_id])
   end
 
 end
